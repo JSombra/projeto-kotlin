54f0e1f6d12dfc29f745d814c167b3e504b6cecf
JSombra <soombra73@gmail.com>
2022-04-22 02:16:38 -0300

    Entendendo estruturas condicionais

--- src/main.kt
+++ src/main.kt
@@ -2,8 +2,14 @@ fun main (){
     val nome = "Jefferson"
     val codigoCliente = 1592
     var saldo = 0.0
-    saldo = saldo + 200
+    saldo += 1
     println("Bem vindo $nome")
     println("O seu codigo de cliente e $codigoCliente")
-    println("Seu saldo é $saldo")
+    println("Seu saldo e $saldo")
+
+    when {
+        saldo > 0.0 -> println("A Conta e positiva")
+        saldo == 0.0 -> println("A conta esta zerada")
+        else -> println("A conta esta negativa")
+    }
 }
