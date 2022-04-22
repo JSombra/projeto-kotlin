72255ca9bfe264a271db8c3d83e3979dec5f6f8d
JSombra <soombra73@gmail.com>
2022-04-22 01:39:52 -0300

    Entendendo a sintaxe de variáveis em Kotlin

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main (){
+    val nome = "Jefferson"
+    val codigoCliente = 1592
+    var saldo = 0.0
+    saldo = saldo + 200
+    println("Bem vindo $nome")
+    println("O seu codigo de cliente e $codigoCliente")
+    println("Seu saldo é $saldo")
+}
