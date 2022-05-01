d1c62819d052a76293cc251434e9f37f09539a2e
JSombra <soombra73@gmail.com>
2022-05-01 12:22:31 -0300

    Criando funções de validação, utilizando array e iniciando em property

--- src/main.kt
+++ src/main.kt
@@ -1,32 +1,76 @@
 fun main() {
-    val ordem = OrdemDeServico()
-    ordem.nome = "Jefferson"
-    println(ordem.nome)
-    println(ordem.codigoDoProduto)
-    println(ordem.valorAPagar)
-
-    val ordem2 = OrdemDeServico()
-    ordem2.nome = "Raquel"
-    println(ordem2.nome)
-    println(ordem2.codigoDoProduto)
-    println(ordem2.valorAPagar)
+    testaCopiasEReferencias()
 }
 
-class OrdemDeServico{
+class OrdemDeServico() {
     var nome = ""
     var codigoCliente = 1
     var idade = (18..80).random()
-    var codigoDoProduto = (1..4).random()
+    var codigoDoProduto = (0..3).random()
+    var valorEmConta = 0.0
     var valorAPagar = 0
+    var senha = "0000"
 
+
+    fun produtoReferenteAoCodigo(codigoDoProduto: Int, valorEmConta: Double) {
+
+        val produto = arrayListOf<String>("Notebook", "Tablet", "Celular", "Monitor")
+        val valorProduto = arrayListOf<Int>(5000, 2000, 6000, 900)
+
+        var produtoSelecionado = produto[codigoDoProduto]
+        var valorProdutoSelecionado = valorProduto[codigoDoProduto]
+        if (valorEmConta >= valorProduto[codigoDoProduto]){
+            println("Produto pode ser comprado. Valor em conta e suficiente")
+        } else {
+            println("Nao possui valor para comprar o produto.")
+        }
+
+        println("--------Produto Selecionado-------")
+        println("Produto: $produtoSelecionado")
+        println("Valor: R$ $valorProdutoSelecionado")
+        println("----------------------------------")
+    }
+
+    fun alteraSenha(novaSenha: String) {
+        if (novaSenha == this.senha) {
+            println("A nova senha cadastrada, e igual a senha padrao. Favor inserir uma senha diferente")
+        } else {
+            this.senha = novaSenha
+            println("Senha alterada com sucesso, sua nova senha e $novaSenha")
+        }
+    }
 }
 
-fun testaCondicoes(saldo: Double) {
-    when {
-        saldo > 0.0 -> println("A Conta e positiva")
-        saldo == 0.0 -> println("A conta esta zerada")
-        else -> println("A conta esta negativa")
-    }
+fun testaCopiasEReferencias() {
+    val ordem = OrdemDeServico()
+    ordem.nome = "Jefferson"
+    ordem.idade = 22
+    ordem.codigoCliente = 1
+    ordem.valorEmConta = 900.0
+
+
+    val ordem2 = OrdemDeServico()
+    ordem2.nome = "Raquel"
+    ordem2.idade = 24
+    ordem2.codigoCliente = 2
+    ordem2.valorEmConta = 3000.0
+
+
+    println(ordem.nome)
+    println(ordem.idade)
+    println(ordem.codigoCliente)
+    ordem.produtoReferenteAoCodigo(ordem.codigoDoProduto, ordem.valorEmConta)
+    println("-----------")
+    ordem.alteraSenha("0001")
+    println("-----------")
+
+    println(ordem2.nome)
+    println(ordem2.idade)
+    println(ordem2.codigoCliente)
+    ordem2.produtoReferenteAoCodigo(ordem2.codigoDoProduto, ordem2.valorEmConta)
+    println("-----------")
+    ordem2.alteraSenha("0002")
+    println("-----------")
 }
 
 fun testaExemplo() {
